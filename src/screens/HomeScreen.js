665c3df1d2b025c300e4fff573096f5ccdef0ca1
zaycation.dev <isaiahthomas098@gmail.com>
2021-03-03 14:14:28 -0800

    resolved Button err

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 import Toc from "../components/Toc";
 import Intro from "../components/Intro";
